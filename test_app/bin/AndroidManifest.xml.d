d97ff058764934365a8174a51394a6bdf4b544b5
lukejaffe1@gmail.com <jaffe.lu@husky.neu.edu>
2015-09-20 14:22:41 -0400

    Remove unneeded files and bin directory (no one should commit bin)

--- test_app/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jaffe5/Projects/capstone/code/sBox/test_app/bin/AndroidManifest.xml : \
-/home/jaffe5/Projects/capstone/code/sBox/test_app/AndroidManifest.xml \
--- test_app/bin/TestAndroidProj-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/jaffe5/Projects/capstone/code/sBox/test_app/bin/TestAndroidProj-debug-unaligned.apk : \
-/home/jaffe5/Projects/capstone/code/sBox/test_app/bin/TestAndroidProj.ap_ \
-/home/jaffe5/Projects/capstone/code/sBox/test_app/bin/classes.dex \
