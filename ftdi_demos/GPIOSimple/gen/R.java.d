62eee571d3c2bc01eae2975cf1b19b36029791d7
lukejaffe1@gmail.com <jaffe.lu@husky.neu.edu>
2015-10-21 17:49:22 -0400

    Add simplified version of GPIODemo with only needed parts

---
+++ ftdi_demos/GPIOSimple/gen/R.java.d
@@ -0,0 +1,13 @@
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/gen/com/GPIODemo/R.java \
+ : /home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/xml/accessory_filter.xml \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/layout/main.xml \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/values/strings.xml \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-mdpi/icon.png \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-hdpi/icon.png \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-hdpi/blue.png \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-hdpi/button_pattern_reset.xml \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-hdpi/image100.png \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-hdpi/image1.png \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-hdpi/button_pattern.xml \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/res/drawable-ldpi/icon.png \
+/home/lucas/Projects/capstone/sBox/ftdi_demos/GPIOSimple/bin/AndroidManifest.xml \
